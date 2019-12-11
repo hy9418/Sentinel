abf02ae56adc484f3ff4594baf5996a6d66fa069
HuYu <huyu@ifugle.com>
2019-12-11 10:10:20 +0800

    compatible redis protocol when using redisson

--- sentinel-extension/sentinel-parameter-flow-control/src/main/java/com/alibaba/csp/sentinel/support/RdSupporter.java
+++ sentinel-extension/sentinel-parameter-flow-control/src/main/java/com/alibaba/csp/sentinel/support/RdSupporter.java
@@ -1,5 +1,6 @@
 package com.alibaba.csp.sentinel.support;
 
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -7,6 +8,7 @@ import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.redisson.config.SingleServerConfig;
+import org.redisson.misc.URIBuilder;
 
 /**
  * @author Wolken Hu
@@ -15,36 +17,13 @@ import org.redisson.config.SingleServerConfig;
  */
 public class RdSupporter {
 
-    private static final String RAP_CLOUD_GATEWAY_CLUSTER_ENABLE =
-            "rap.cloud.gateway.cluster.enable";
     private static final String REDIS_COMMAND_PREFIX = "rap-cloud-gateway:";
     private static final String REDIS_SERVER = "rap.redis.server";
     private static final String REDIS_AUTH = "rap.redis.auth.password";
     private static final Map<String, CommandResource> COMMANDS_CACHE = new HashMap<>();
+    private static final String REDIS_PROTOCOL = "redis://";
     private static AtomicBoolean init = new AtomicBoolean(false);
     private static RedissonClient redissonClient;
-
-    public static synchronized void initialize() {
-        if (init.compareAndSet(false, true)) {
-            if (!Boolean.valueOf(System.getProperty(RAP_CLOUD_GATEWAY_CLUSTER_ENABLE, "true"))) {
-                return;
-            }
-            String server = System.getProperty(REDIS_SERVER);
-            if (server == null || "".equals(server) || !server.contains(":")) {
-                throw new IllegalArgumentException(
-                        "Redis server [rap.redis.server] not set. Format - <host>:<port>");
-            }
-            String password = System.getProperty(REDIS_AUTH);
-            Config config = new Config();
-            //TODO 兼容协议
-            SingleServerConfig singleServerConfig = config.useSingleServer();
-            singleServerConfig.setAddress(server);
-            if (password != null) {
-                singleServerConfig.setPassword(password);
-            }
-            redissonClient = Redisson.create(config);
-        }
-    }
 
     public static CommandResource path(String path) {
         if (!init.get()) {
@@ -65,5 +44,54 @@ public class RdSupporter {
             }
         }
         return COMMANDS_CACHE.get(prefix);
+    }
+
+    public static synchronized void initialize() {
+        if (init.compareAndSet(false, true)) {
+            String server = System.getProperty(REDIS_SERVER);
+            if (server == null || "".equals(server) || !server.contains(":")) {
+                throw new IllegalArgumentException(
+                        "Redis server [rap.redis.server] not set. Format - <host>:<port>");
+            }
+            String password = System.getProperty(REDIS_AUTH);
+            Config config = new Config();
+            SingleServerConfig singleServerConfig = config.useSingleServer();
+            singleServerConfig.setAddress(getOrAssembleProtocol(server));
+            if (password != null) {
+                singleServerConfig.setPassword(password);
+            }
+            redissonClient = Redisson.create(config);
+        }
+    }
+
+    private static String getOrAssembleProtocol(String raw) {
+        URI uri;
+        boolean haveProtocol = raw.startsWith(REDIS_PROTOCOL);
+        String assembled = haveProtocol ? raw : REDIS_PROTOCOL + raw;
+        String result = raw;
+        if (compatibleRedissonVersion()) {
+            uri = URIBuilder.create(assembled);
+            if (uri.getHost() != null && uri.getPort() < 0xffff && uri.getPort() > 0) {
+                result = assembled;
+            }
+        } else {
+            result = excludeProtocol(REDIS_PROTOCOL, raw);
+        }
+        return result;
+    }
+
+    private static boolean compatibleRedissonVersion() {
+        boolean b;
+        try {
+            Class.forName("org.redisson.misc.URIBuilder");
+            b = true;
+        } catch (ClassNotFoundException e) {
+            b = false;
+        }
+        return b;
+    }
+
+    private static String excludeProtocol(String protocol, String raw) {
+        return raw.replace(protocol, "");
     }
 }
